e72ab5269a1c7aafbdf97f18ac78e274f810b5a1
Keval Goyani <keval.g@simformsolutions.com>
2022-05-03 13:03:03 +0530

    feat: TE5-T609 Integration of Splash screen - Add Splash screen on Android & iOS, Changes Icon and Icon Name on both

--- app/theme/Metrics.ts
+++ app/theme/Metrics.ts
@@ -6,11 +6,11 @@ const guidelineBaseWidth = 375;
 
 const guidelineBaseHeight = 812;
 
-const horizontalScale = size => (width / guidelineBaseWidth) * size;
+const horizontalScale = (size: number) => (width / guidelineBaseWidth) * size;
 
-const verticalScale = size => (height / guidelineBaseHeight) * size;
+const verticalScale = (size: number) => (height / guidelineBaseHeight) * size;
 
-const moderateScale = (size, factor = 0.5) =>
+const moderateScale = (size: number, factor = 0.5) =>
   size + (horizontalScale(size) - size) * factor;
 
 export {horizontalScale, verticalScale, moderateScale};
